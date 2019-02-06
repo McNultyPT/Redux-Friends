80409aeb53152038787c835900540b9fa9384fe9
McNultyPT <mcnultypt@gmail.com>
2019-02-06 15:44:57 -0800

    adding return statements to reducer function

--- friends/src/reducers/index.js
+++ friends/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { FETCH_FRIEND_START, FETCH_FRIEND_SUCCESS, FETCH_FRIEND_ERROR } from '../actions';
 
 const initialState = {
+    friends: [],
     fetchingFriends: false,
     friendsFetched: false,
     friendAdded: false,
@@ -12,15 +13,23 @@ function reducer(state = initialState, action) {
     switch(action.type) {
         case FETCH_FRIEND_START:
             return {
-
+                ...state,
+                fetchingFriends: true,
+                friendsFetched: false
         };
         case FETCH_FRIEND_SUCCESS:
             return {
-
+                ...state,
+                fetchingFriends: false,
+                friendsFetched: true,
+                friends: action.payload
         };
         case FETCH_FRIEND_ERROR:
             return {
-
+                ...state,
+                fetchingFriends: false,
+                friendsFetched: false,
+                error: action.payload
         };
         default:
             return state;
